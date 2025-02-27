0dfa3a848621ab0b2021210b3e77c60a29a9633c
K M Marziul Karim <marziulrafi@gmail.com>
2025-02-27 19:50:37 +0600

    addEventListener

--- events.js
+++ events.js
@@ -12,10 +12,20 @@ btnMakeBlue.onclick = function makeBlue() {
     document.body.style.backgroundColor = 'blue'
 }
 
-// Option 4
+// Option 3 (Different)
 const btnMakePlum = document.getElementById('btn-plum');
 btnMakePlum.onclick = makePlum;
 
 function makePlum() {
     document.body.style.backgroundColor = 'plum'
 }
+
+// Option 4
+document.getElementById('btn-green').addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+})
+
+// Mouseover
+document.getElementById('btn-gold').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = 'gold'
+})
